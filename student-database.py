318ae498c941b71c968881dc7d1628db613182bd
ritesh pathak <me@ritesh.com>
2020-04-01 13:29:03 +0530

    file update with 3 new dictonary

--- student-database.py
+++ student-database.py
@@ -1,29 +1,58 @@
 print('''Welcome to student Data base
-         Press 1 to get user detail by roll number
-         Press 2 to get user detail by first name
-         Press 3 to get user detail by email address
-         Press 4 to delete user detail by roll number
-         Press 5 to show all user
-         Press 6 to update user detail by roll number
-         Press 7 to delete user detail by roll number
-         Press 8 to find user siblings by father name
-         Press 9 to sort user detail by last name
-         Press 10 to sort user detail by roll number
-         Press 11 to validate user inputs
+         1.Get user detail by roll number
+         2.Get user detail by first name
+         3.Get user detail by email address
+         4.Delete user detail by roll number
+         5.Show all user
+         6.Update user detail by roll number
+         7.Delete user detail by roll number
+         8.Find user siblings by father name
+         9.Sort user detail by last name
+         10.Sort user detail by roll number
+         11.Validate user inputs
 
          ''')
-student1 = input(str('Enter your choice: '))
 
-if student1 == '1':
+student1 = input(str("Enter the details: "))
+
+if student1 == 'IN001':
     print(rahul)
-elif student1 == '2':
+elif student1 == 'rahul':
     print(rahul)
-elif student1 == '3':
+elif student1 == 'singh':
     print(rahul)
+if student1 == 'IN002':
+    print(dave)
+elif student1 == 'dave':
+    print(dave)
+elif student1 == 'smith':
+    print(dave)
+if student1 == 'IN003':
+    print(ava)
+elif student1 == 'ava':
+    print(ava)
+elif student1 == 'smith':
+    print(ava)
+
     rahul = {
-            'first_name': 'rahul',
-            'last_name': 'singh',
-            'father_name': 'shyam singh',
-            'date_of_birth': '1993',
-            'email_address': 'rahul@in.com',
-            'roll_number': 'IN001'}
+        'first_name': 'rahul',
+        'last_name': 'singh',
+        'father_name': 'shyam singh',
+        'date_of_birth': '1993',
+        'email_address': 'rahul@in.com',
+        'roll_number': 'IN001'}
+
+    dave = {
+        'first_name': 'dave',
+        'last_name': 'smith',
+        'father_name': 'john smith',
+        'date_of_birth': '1990',
+        'email_address': 'dave@in.com',
+        'roll_number': 'IN002'}
+    ava = {
+        'first_name': 'ava',
+        'last_name': 'smith',
+        'father_name': 'john smith',
+        'date_of_birth': '1992',
+        'email_address': 'ava@in.com',
+        'roll_number': 'IN003'}
